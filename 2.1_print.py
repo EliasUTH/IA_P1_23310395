10ee62bb758950d407dfdb971f1f543f08650e6a
EliasUTH <134228401+EliasUTH@users.noreply.github.com>
2026-02-24 15:05:56 -0600

    Archivo 2.1_print.py
    Creación del primer archivo .py, tema básico sobre sintaxis de print

---
+++ 2.1_print.py
@@ -0,0 +1,11 @@
+# Usar "#" para comentar
+# print("") es la sintaxis correcta
+print("Primer línea")
+print()
+print("Tercera \n Cuarta")
+# Cada print imprime una mueva línea
+# \n es un cambio de línea
+print("arg1" , "arg2", sep="-", end=" ")# Más de un argumento, misma línea
+print("Fin.")
+# sep="" para separaciones entre argumentos de un print
+# end="" para finales de línea de un print
