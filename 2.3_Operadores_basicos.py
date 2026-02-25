65bdb7461a2a2c07fdce767c12ca6566bed8ad60
EliasUTH <134228401+EliasUTH@users.noreply.github.com>
2026-02-24 18:32:41 -0600

    Archivo 2.3, operadores básicos
    Tema de operadores básicos, operaciones aritméticas

---
+++ 2.3_Operadores_basicos.py
@@ -0,0 +1,8 @@
+print("Suma","6", '+', "2", '=', 6+2, sep=" ")
+print("Resta","6", '-', "2", '=', 6-2, sep=" ")
+print("Producto","6", '*', "2", '=', 6*2, sep=" ")
+print("División","6.5", '*', "2", '=', 6.5/2, sep=" ")
+print("Módulo","6", '%', "2", '=', 6%2, sep=" ")
+print("Exponenciación","6", '**', "2", '=', 6**2, sep=" ")
+print("División entera","6.5", '//', "2", '=', 6.5//2, sep=" ")
+#Las operaciones aritméticas siguen la jerarquía de operaciones
