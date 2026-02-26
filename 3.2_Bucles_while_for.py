f010582e7121baaa8f316fc6b53b2ef1e016ee01
EliasUTH <134228401+EliasUTH@users.noreply.github.com>
2026-02-26 12:46:54 -0600

    3.2 Bucles while y for

---
+++ 3.2_Bucles_while_for.py
@@ -0,0 +1,26 @@
+var = ""
+word_without_vowels = ""
+print("_Devorador de vocales_") #Función del código
+while var != "n": #Mientras la condición se cumpla seguirá dentro del bucle
+    user_word = input("Ingrese una palabra... ")
+    user_word = user_word.upper() #La palabra ingresada pasa a mayúsculas
+    for letter in user_word:
+        if letter == 'A':
+            continue
+        elif letter == 'E':
+            continue
+        elif letter == 'I':
+            continue
+        elif letter == 'O':
+            continue
+        elif letter == 'U':
+            continue
+        else: 
+            word_without_vowels += letter
+    print(word_without_vowels)
+    word_without_vowels = ""
+    var = input("¿Continuamos? Escribe s para continuar, n para salir.")
+    if var != 's' and var !='n': #And equivalente a &&
+        print("De esa no hay.")
+        continue
+print("Hasta luego.")
