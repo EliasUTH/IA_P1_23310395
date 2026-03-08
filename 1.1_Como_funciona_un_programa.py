5caa44bfa8d2caab9e0f72854ae9a377bff88261
EliasUTH <134228401+EliasUTH@users.noreply.github.com>
2026-03-08 12:26:55 -0600

    Archivos que faltaban
    Cambio de nombre de hola mundo, y creación de archivos sobre teoría que faltaban en el repositorio

---
+++ 1.1_Como_funciona_un_programa.py
@@ -0,0 +1,29 @@
+#Como funciona un programa de computadora
+var = ""
+var = input("¿Sabes que es un lenguaje? S o N...\n")
+if var == 'S' or 's':
+    print("Que bueno, pero de todas formas te lo diré.")
+else:
+    print("No te preocupes, yo te digo.")
+print("Un lenguaje es un medio (y una herramienta) para expresar y registrar pensamientos. Hay muchos lenguajes a nuestro alrededor. Algunos de ellos no requieren ni hablar ni escribir, como el lenguaje corporal.")
+var = ""
+var = input("\n¿Cómo se llama el lenguaje de una computadora?")
+if var == "maquina" or "Maquina" or "máquina" or "Máquina":
+    print("Así es, le sabes.")
+else:
+    print("Es lenguaje maquina.")
+var = ""
+var = input("\n¿Sabes cuáles son los 4 elementos de un lenguaje? S o N")
+if var == 'S' or 's':
+    print("Que bueno, pero de todas formas te lo diré.")
+else:
+    print("No te preocupes, yo te digo.")
+print("Alfabeto, léxico, sintaxis y semántica.")
+var = ""
+var = input("\n¿Sabes cuál es la diferencia entre compilación e interpretación? S o N")
+if var == 'S' or 's':
+    print("Que bueno, pero de todas formas te lo diré.")
+else:
+    print("No te preocupes, yo te digo.")
+print("\nCompilación: Imagina que escribes un libro en español y quieres que un lector en China lo lea. Un compilador actúa como un traductor que toma todo el libro completo, lo traduce al chino y te entrega un libro nuevo ya impreso en ese idioma. El lector chino ya no necesita al traductor; solo lee el libro nuevo.")
+print("Interpretación: Ahora imagina que estás dando una conferencia en español ante un público en China. Un intérprete simultáneo escucha lo que dices oración por oración y lo traduce al chino en tiempo real.")
---
+++ 1.2_Que_es_Python.py
@@ -0,0 +1,16 @@
+var = ""
+var = input("¿Python es un animal? S o N...")
+if var == 'S' or 's':
+    print("En este caso no, deja te explico...")
+else:
+    print("Ajá, no es un animal en este contexto...")
+print("Python es un lenguaje de programación de alto nivel, interpretado, orientado a objetos y de uso generalizado con semántica dinámica, que se utiliza para la programación de propósito general.")
+var = ""
+print("\nUna de las características sorprendentes de Python es el hecho de que en realidad es el trabajo de una persona. Por lo general, los grandes lenguajes de programación son desarrollados y publicados por grandes compañías que emplean a muchos profesionales")
+var = input("a)Guido van Rossum b)Elías Torres c)Un japonés Elige una, a, b o c")
+if var == 'A' or 'a':
+    print("Vaya, si le sabes.")
+elif var == 'b' or 'B':
+    print("Gracias por la confianza, pero no...")
+print("Python fue creado por Guido van Rossum, nacido en 1956 en Haarlem, Países Bajos.")
+
